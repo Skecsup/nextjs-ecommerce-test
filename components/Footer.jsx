129a1c901330dd2b8bd197f8914271cb4fcf596c
Skecsup <kicsindy.julius@gmail.com>
2022-07-29 14:25:55 +0200

    home page completed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { AiOutlineInstagram, AiOutlineFacebook } from "react-icons/ai";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 KICSINDY All rights reserved ©</p>
+      <p className="icons">
+        <AiOutlineInstagram />
+        <AiOutlineFacebook />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import { Product, FooterBanner, HeroBanner } from "../components";
-const Home = () => {
+import { client } from "../lib/client";
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product1", "Product2"].map((product) => product)}
+        {products?.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
